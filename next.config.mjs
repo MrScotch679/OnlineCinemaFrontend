190ca81d33ae108dd567d9d8bc50574a0541fd14
Ulad <vladis67948@gmail.com>
2024-05-06 19:46:48 +0300

    feat(app): added config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_API_URL,
+		APP_ENV: process.env.REACT_APP_ENV
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:3000/api/:path*`
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:3000/uploads/:path*`
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
